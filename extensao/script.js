e9c813f2bb0713968f1d1e8adfc96b7d150e45d8
RoberthOvelar <roberth.fontes@ufms.br>
2022-03-23 22:30:40 -0400

    Extensao quase pronta

--- extensao/script.js
+++ extensao/script.js
@@ -1,15 +1,46 @@
-const itens = document.querySelectorAll(".coursebox")
+//Selecionando divs que queremos ordenar
+var itens = document.querySelectorAll(".courses .coursebox")
+cursos = document.querySelector(".courses") //div geral dos cursos
 
-var cont = 0
-while(cont < itens.length){
-    console.log(itens[cont].querySelector(".aalink").innerHTML);
-    cont++
+//convertendo NodeList para ArrayList
+var itensOrdenados = [].map.call(itens, function(element){return element})
+
+//Ordenando com sort
+itensOrdenados.sort(function(a, b){
+    //Critério de ordenação será a partir dos últimos 6 elementos da string
+    var x = a.querySelector(".aalink").innerHTML.slice(a.querySelector(".aalink").innerHTML.length-6)
+    var y = b.querySelector(".aalink").innerHTML.slice(b.querySelector(".aalink").innerHTML.length-6)
+    if(x > y){return -1}
+    if(x < y){return 1}
+    return 0
+})
+
+//Chamando a função para adicionar a nova lista ordenada na página HTML
+adicionaItensOrdenados()
+
+function adicionaItensOrdenados(){
+    var semestre = itensOrdenados[0].querySelector(".aalink").innerHTML.substr(itensOrdenados[0].querySelector(".aalink").innerHTML.length-6)
+    //cursos.appendChild(document.createTextNode(semestre))
+
+    for(var i = 0; i < itensOrdenados.length; i++){
+        var len = itensOrdenados[i].querySelector(".aalink").innerHTML.length
+        //Zebrando tabelas
+        if(i%2 == 0){itensOrdenados[i].style.backgroundColor = "rgba(0,0,0,.05)"}
+        else {itensOrdenados[i].style.backgroundColor = "rgba(255,255,255)"}
+        
+        if(itensOrdenados[i].querySelector(".aalink").innerHTML.slice(len-6) != semestre){
+            semestre = itensOrdenados[i].querySelector(".aalink").innerHTML.slice(len-6)
+            cursos.appendChild(document.createElement("hr")) //adicionar após cada semestre
+        }
+        cursos.appendChild(itensOrdenados[i])
+    }
 }
 
-console.log("================== DEPOIS DA ORDENAÇÃO ==================")
+
+
+
 
 //Como ordenar?
 
 //console.log(itens[2].querySelector(".aalink").innerHTML.substr(itens[2].querySelector(".aalink").innerHTML.length-6, itens[2].querySelector(".aalink").innerHTML.length))
 /*itens[0].querySelector(".aalink").innerHTML.substr(itens[0].querySelector(".aalink").innerHTML.length-6, itens[0].querySelector(".aalink").innerHTML.length)*/ 
-
